cd1f53791dc6de5e10c69285816d291ae2a1493d
Aaron Lee <aaron.lee@wagwalking.com>
2019-02-08 17:32:51 -0800

    fix checksum schema

--- install.sql
+++ install.sql
@@ -5,7 +5,7 @@ USE slow_query_log;
 
 -- Create the global query review table
 CREATE TABLE `global_query_review` (
-  `checksum` varchar(32) unsigned NOT NULL,
+  `checksum` varchar(32) NOT NULL,
   `fingerprint` text NOT NULL,
   `sample` longtext NOT NULL,
   `first_seen` datetime DEFAULT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE `global_query_review` (
 CREATE TABLE `global_query_review_history` (
   `hostname_max` varchar(64) NOT NULL,
   `db_max` varchar(64) DEFAULT NULL,
-  `checksum` varchar(32) unsigned NOT NULL,
+  `checksum` varchar(32) NOT NULL,
   `sample` longtext NOT NULL,
   `ts_min` datetime NOT NULL DEFAULT '1990-01-01 00:00:00',
   `ts_max` datetime NOT NULL DEFAULT '1990-01-01 00:00:00',
